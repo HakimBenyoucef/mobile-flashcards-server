3da6ea9c975cc24fa0b79bc9e5622e98f36c6476
Hakim Benyoucef <benyoucef.hakim@gmail.com>
2021-03-29 01:02:02 +0200

    add quiz API

--- routes/quizRouter.js
+++ routes/quizRouter.js
@@ -1,21 +1,65 @@
 const express = require("express");
 const quizRouter = express.Router();
 
+const Quiz = require("../models/quiz");
+
 // Getting all
-quizRouter.get("/", (req, res) => {
-  res.send("Hello world");
+quizRouter.get("/", async (req, res) => {
+    try {
+        const quizzes = await Quiz.find({});
+        res.json(quizzes);
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
 });
 
 // Getting one
-quizRouter.get("/:id", (req, res) => {
-  res.send(req.params.id);
+quizRouter.get("/:name", getQuiz, (req, res) => {
+    res.json(res.quiz);
 });
+
 // Creating one
-quizRouter.post("/", (req, res) => {});
+quizRouter.post("/", async (req, res) => {
+    let quiz = await Quiz.findOne({ name: req.body.name });
+  if (quiz != null) {
+    return res.status(409).json({ message: "Quiz already exists" });
+  }
+  quiz = new Quiz({
+    name: req.body.name,
+  });
+  try {
+    const newQuiz = await quiz.save();
+    res.status(201).json(newQuiz);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
 // updating one
 quizRouter.patch("/", (req, res) => {});
 
 // deleting one
-quizRouter.delete("/:id", (req, res) => {});
+quizRouter.delete("/:name", getQuiz, async (req, res) => {
+    try {
+        await res.quiz.remove();  
+        res.json({message: "Quiz deleted successfully"})
+      } catch (error) {
+        res.status(500).json({ message: error.message });
+      }
+});
+
+
+async function getQuiz(req, res, next) {
+    let quiz;
+    try {
+      quiz = await Quiz.findOne({ name: req.params.name });
+      if (quiz == null) {
+        return res.status(404).json({ message: "Quiz not found" });
+      }
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+    res.quiz = quiz;
+    next();
+  }
 
 module.exports = quizRouter;
